4f67c25d66482009535132d8ec6226d781e362a2
Bettenoch <kipngenoemmanuel479@gmail.com>
2023-08-02 22:16:47 +0300

    Initialize React app with Redux store and BrowserRouter and add import style.css

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import './styles/index.css';
 import App from './components/App';
 import store from './redux/store';
 
