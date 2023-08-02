91b545051e0e34b072cdb621a02ba8b0ea383180
Bettenoch <kipngenoemmanuel479@gmail.com>
2023-08-02 16:38:57 +0300

    Refactor Book component and add prop type validation

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,47 +1,45 @@
-import React from 'react';
 import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
-import { removeBook } from '../redux/books/booksSlice';
+import { removeBookFromAPI } from '../redux/books/booksSlice';
 
-const Book = ({ itemId, title, author }) => {
+const Book = ({ book }) => {
   const dispatch = useDispatch();
-
-  const handleRemoveBook = () => {
-    dispatch(removeBook({ id: itemId })); // Use itemId instead of id
-  };
-
+  const {
+    id, title, author, category,
+  } = book;
   return (
-    <div className="eachBook">
-      <div className="bookDetails">
-        <p className="bookTitle">{title}</p>
-        <p>{author}</p>
-        <div className="commentRemoveEditDiv">
-          <p className="commentRemoveEditEffects">Comments</p>
-          <button
-            type="button"
-            className="commentRemoveEditEffects removeButton"
-            onClick={handleRemoveBook}
-          >
-            Remove
-          </button>
-          <p className="commentRemoveEditEffects">Edit</p>
+    <div className="eachBookDiv">
+      <div className="eachBook">
+        <div className="bookDetails">
+          <p className="genre">{category}</p>
+          <p className="bookTitle">{title}</p>
+          <p>{author}</p>
+          <div className="commentRemoveEditDiv">
+            <p className="commentRemoveEditEffects">Comments</p>
+            <button
+              type="button"
+              className="removeBookButton"
+              onClick={() => dispatch(removeBookFromAPI(id))}
+            >
+              Remove
+            </button>
+            <p className="commentRemoveEditEffects">Edit</p>
+          </div>
         </div>
-      </div>
-      <div className="completedRing">
-        <div className="ring">.</div>
-        <div>
-          <h2 className="percent">64%</h2>
-          <span>Completed</span>
+        <div className="completedRing">
+          <div className="ring">.</div>
+          <div>
+            <h2 className="percent">64%</h2>
+            <span>Completed</span>
+          </div>
         </div>
-      </div>
-      <div className="currentChapter">
-        <div className="line">.</div>
-        <div>
-          <p className="chapterDetails">CURRENT CHAPTER</p>
-          <p className="chapterNumber">Chapter 17</p>
-          <button type="button" className="progressButton">
-            UPDATE PROGRESS
-          </button>
+        <div className="currentChapter">
+          <div className="line">.</div>
+          <div>
+            <p className="chapterDetails">CURRENT CHAPTER</p>
+            <p className="chapterNumber">Chapter 17</p>
+            <button type="button" className="progressButton">UPDATE PROGRESS</button>
+          </div>
         </div>
       </div>
     </div>
@@ -49,9 +47,12 @@ const Book = ({ itemId, title, author }) => {
 };
 
 Book.propTypes = {
-  itemId: PropTypes.string.isRequired,
-  title: PropTypes.string.isRequired,
-  author: PropTypes.string.isRequired,
+  book: PropTypes.shape({
+    id: PropTypes.string,
+    title: PropTypes.string,
+    author: PropTypes.string,
+    category: PropTypes.string,
+  }).isRequired,
 };
 
 export default Book;
