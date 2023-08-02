9f6953bb140d9d4cd6d5a140bccad42c3861adde
Bettenoch <kipngenoemmanuel479@gmail.com>
2023-08-02 16:39:43 +0300

    Render list of books and add form component

--- src/components/Books.js
+++ src/components/Books.js
@@ -4,13 +4,13 @@ import Book from './Book';
 import Form from './Form';
 
 const Books = () => {
-  const books = useSelector((state) => state.books);
+  const books = useSelector((state) => state.books.booksList); // Access the booksList state
 
   return (
     <div className="booksDiv">
       <div className="eachBookDiv">
         {books.map((book) => (
-          <Book key={book.item_id} itemId={book.item_id} title={book.title} author={book.author} />
+          <Book key={book.id} book={book} /> // Add the unique key prop using the book's id
         ))}
       </div>
       <Form />
